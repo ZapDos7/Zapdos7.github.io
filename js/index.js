8bd820a5c5306961148026a22730faf2a0a0a244
ZapDos7 <ZapDos7@users.noreply.github.com>
2020-10-01 14:19:02 +0300

    Mobile View Fixes

--- js/index.js
+++ js/index.js
@@ -30,13 +30,33 @@ $(document).ready(function() {
   })
 });
 
-//close menu upon clicking a li element and moving there:
+//close menu upon clicking a nav>ul>li>a element and moving there:
 $(document).ready(function() {
   $(".nav").find("a").on("click", closeMenu);
 });
-
 function closeMenu()
 {
   $(".nav").removeClass("open");
   //console.log("time to remove OPEN class from menu");
+}
+
+/*Button to top by W3S */
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
